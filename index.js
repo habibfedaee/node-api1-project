67364e53cec6fd0d3e6e70768ba1c0c33ad4798a
habibfedaee <habib.fedaee@gmail.com>
2023-05-08 21:33:31 -0700

    get going

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const server = require('./api/server');
+const server = require("./api/server");
 
 const port = 9000;
 
 // START YOUR SERVER HERE
+//console.log("Hey");
+
+server.listen(port, () => {
+  console.log("listening on", port);
+});
