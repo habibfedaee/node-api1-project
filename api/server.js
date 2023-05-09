0ca33f8acfecd86a416b5067c2c1e060cbbe0c06
habibfedaee <habib.fedaee@gmail.com>
2023-05-08 22:06:14 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const server = express();
+const User = require("./users/model");
 
+// get all users
 server.get("/api/users", (req, res) => {
   //console.log("gettting all users");
-  res.json("usersss!!!");
+  //res.json("usersss!!!");
+
+  User.find()
+    .then((users) => {
+      //console.log(users);
+      res.json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "error gettting users",
+        err: err.message,
+        stack: err.stack,
+      });
+    });
+});
+
+// get user by id:
+server.get("/api/users/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      //console.log(user);
+      res.json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "error gettting user",
+        err: err.message,
+        stack: err.stack,
+      });
+    });
 });
 
 server.use("*", (req, res) => {
