7b2f6eeeeca47713eb6dab91bd9d9a45f9e595d0
habibfedaee <habib.fedaee@gmail.com>
2023-05-08 22:58:35 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const server = express();
 const User = require("./users/model");
+server.use(express.json());
 
-// get all users
+// creating a user:----------------------
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (!user.name || !user.bio) {
+    //
+    res.status(400).json({
+      message: "Please provide name and bio for the user ",
+    });
+  } else {
+    //
+    User.insert(user)
+      .then((createdUser) => {
+        res.status(201).json(createdUser);
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "error creating user",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  }
+});
+
+// get all users -------------------------
 server.get("/api/users", (req, res) => {
   User.find()
     .then((users) => {
@@ -18,7 +43,7 @@ server.get("/api/users", (req, res) => {
     });
 });
 
-// get user by id:
+// get user by id:--------------------------
 server.get("/api/users/:id", (req, res) => {
   User.findById(req.params.id)
     .then((user) => {
